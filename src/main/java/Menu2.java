5421c73679e51e2a29e746c13da4dfedf90a4562
younknight <asdqwer132@naver.com>
2022-03-10 17:07:21 +0900

    Professor Samuel Oak's script update

--- src/main/java/Menu2.java
+++ src/main/java/Menu2.java
@@ -1,9 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Menu2{
 
     public Menu2() {
     }
 
-    public void MrOak() {
+    public static void ProfessorOak(){
+        Scanner scanner = new Scanner(System.in);
+            try{
+                //파일 객체 생성
+                File file = new File("src/main/java/oak");
+                //입력 스트림 생성
+                FileReader filereader = new FileReader(file);
+                //입력 버퍼 생성
+                BufferedReader bufReader = new BufferedReader(filereader);
+                String line = "";
+                while((line = bufReader.readLine()) != null){
+                    System.out.println(line);
 
-    }
+
+                    if(line.equals("이름을 입력하세요:")) {
+                        String name = scanner.next();
+                        System.out.println("이름은 " + name + "입니다.");
+                    }
+
+                }
+                //.readLine()은 끝에 개행문자를 읽지 않는다.
+                bufReader.close();
+            }catch (FileNotFoundException e) {
+                // TODO: handle exception
+            }catch(IOException e){
+                System.out.println(e);
+            }
+        }
+
+
 }
